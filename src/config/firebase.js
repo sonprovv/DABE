4290acdeb90ce471f33dc5c892bfcc7619a24162
Tzei <126045215+Kain411@users.noreply.github.com>
2025-08-13 22:16:31 +0700

    Update 1 firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -4,17 +4,13 @@ dotenv.config();
 
 const serviceAccount = JSON.parse(process.env.FB);
 
-const storageBucket = 'jobs-4c9e3.appspot.com';
-
 if (!admin.apps.length) {
     admin.initializeApp({
         credential: admin.credential.cert(serviceAccount),
-        storageBucket: storageBucket
     })
 }
 
 const auth = admin.auth();
 const db = admin.firestore();
-const bucket = admin.storage().bucket();
 
-module.exports = { db, auth, admin, bucket };
+module.exports = { db, auth, admin };
