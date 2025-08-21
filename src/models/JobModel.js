e6f24e8f78e72705ddca0560621e6bc83317c4e9
Tzei <126045215+Kain411@users.noreply.github.com>
2025-08-21 21:27:08 +0700

    Update JobModel.js

--- src/models/JobModel.js
+++ src/models/JobModel.js
@@ -1,30 +1,32 @@
 class JobModel {
-    constructor(uid, userID, serviceType, workerQuantity, status, price, isWeek, dayOfWeek, createdAt) {
+    constructor(uid, userID, serviceType, startTime, endTime, workerQuantity, price, isWeek, dayOfWeek, createdAt, status) {
         this.uid = uid;
         this.userID = userID;
         this.serviceType = serviceType;
+        this.startTime = startTime;
+        this.endTime = endTime;
         this.workerQuantity = workerQuantity;
-        this.status = status;
         this.price = price;
         this.isWeek = isWeek;
         this.dayOfWeek = dayOfWeek;
         this.createdAt = createdAt;
+        this.status = status;
     }
 }
 
 class CleaningJobModel extends JobModel {
-    constructor(uid, userID, serviceType, workerQuantity, status, price, isWeek, dayOfWeek, durationID, services, isCooking, isIroning) {
-        super(uid, userID, serviceType, workerQuantity, status, price, isWeek, dayOfWeek);
+    constructor(uid, userID, serviceType, startTime, endTime, workerQuantity, price, isWeek, dayOfWeek, createdAt, status, durationID, services, isCooking, isIroning) {
+        super(uid, userID, serviceType, startTime, endTime, workerQuantity, price, isWeek, dayOfWeek, createdAt, status);
         this.durationID = durationID;
-        this.services = services; //List serviceID
+        this.services = services;
         this.isCooking = isCooking;
-        this.isIroning = this.isIroning;
+        this.isIroning = isIroning;
     }
 }
 
 class HealthcareJobModel extends JobModel {
-    constructor(uid, userID, serviceType, workerQuantity, status, price, isWeek, dayOfWeek, shiftID, isWeek, services) {
-        super(uid, userID, serviceType, workerQuantity, status, price, isWeek, dayOfWeek);
+    constructor(uid, userID, serviceType, startTime, endTime, workerQuantity, price, isWeek, dayOfWeek, createdAt, status, shiftID, isWeek, services) {
+        super(uid, userID, serviceType, startTime, endTime, workerQuantity, price, isWeek, dayOfWeek, createdAt, status);
         this.shiftID = shiftID;
         this.services = services;
     }
