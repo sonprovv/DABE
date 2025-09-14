bf3c596ed095a7a423c102394b6881dbeaef0a76
Tên của bạn <kain411thien@gmail.com>
2025-09-14 20:51:43 +0700

    Router

--- src/routes/OrderRouter.js
+++ src/routes/OrderRouter.js
@@ -6,8 +6,7 @@ const { checkPermission } = require('../middleware/checkPermission');
 
 router.post('/create', verifyToken, checkPermission(['worker']), createOrder);
 
-// router.get('/:jobID', verifyToken, checkPermission(['user']), getOrdersByJobID);
-router.get('/:jobID', getOrdersByJobID);
+router.get('/:jobID', verifyToken, checkPermission(['user']), getOrdersByJobID);
 
 router.get('/worker/:workerID', verifyToken, checkPermission(['worker']), getOrdersByWorkerID);
 
