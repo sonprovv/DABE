386780cd7dbdc55ca29165f7b39f008639739ddf
Tên của bạn <kain411thien@gmail.com>
2025-09-27 17:33:21 +0700

    Payment for User

--- src/controllers/PaymentController.js
+++ src/controllers/PaymentController.js
@@ -1,8 +1,7 @@
 const { checkPaymentNotification } = require("../notifications/PaymentNotification");
 const AccountService = require("../services/AccountService");
-const OrderService = require("../services/OrderService");
 const PaymentService = require("../services/PaymentService");
-const { failResponse, successResponse } = require("../utils/response");
+const { failResponse } = require("../utils/response");
 
 const checkPayment = async (req, res) => {
 
@@ -21,31 +20,16 @@ const checkPayment = async (req, res) => {
         const jobID = des.substring(28, 48);
         const serviceType = des.substring(48);
         const amount = req.body.transferAmount;
-        console.log(clientID)
-        console.log(jobID)
-        console.log(serviceType)
-        console.log(amount)
 
         try {
             const accountDoc = await AccountService.getByUID(clientID);
             if (accountDoc.role==='user') {
                 console.log('create Payment User')
                 await PaymentService.createPayment(clientID, jobID, serviceType);
+                await checkPaymentNotification(clientID, jobID, serviceType, amount);
             }
-            else if (accountDoc.role==='worker') {
-                console.log('update Payment Worker');
-                await OrderService.updatePayment(clientID, jobID);
-            }
-            else {
-                return failResponse(res, 500, 'Không cho phép');
-            }
-
-            await checkPaymentNotification(clientID, jobID, serviceType, amount);
-
-            return successResponse(res, 200, 'Thành công');
         } catch (err) {
             console.log(err.message);
-            return failResponse(res, 500, err.message);
         }
     }
 }
